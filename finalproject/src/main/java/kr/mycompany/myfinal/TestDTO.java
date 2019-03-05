48264231e59e7a244fd423d65d5059c02d2c5f67
user2 <user2@KH_G>
2019-03-05 20:47:56 +0900

    ㅇ

--- finalproject/src/main/java/kr/mycompany/myfinal/TestDTO.java
+++ finalproject/src/main/java/kr/mycompany/myfinal/TestDTO.java
@@ -3,11 +3,6 @@ package kr.mycompany.myfinal;
 public class TestDTO {
 	private String pp;
 	public void process() {
-<<<<<<< HEAD
-		System.out.println();
-		System.out.println();
-=======
-		System.out.println("안녕");
->>>>>>> branch 'Sonkih' of https://github.com/khpremium/fianl.git
+		System.out.println("유성은");
 	}
 }
