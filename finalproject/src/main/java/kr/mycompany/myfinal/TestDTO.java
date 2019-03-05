f270b000f98bd4569a49ef657e87971cb3179b16
Sonkih <rlghks1992@naver.com>
2019-03-05 21:11:08 +0900

    테스틈ㄴㅇ

--- finalproject/src/main/java/kr/mycompany/myfinal/TestDTO.java
+++ finalproject/src/main/java/kr/mycompany/myfinal/TestDTO.java
@@ -2,7 +2,11 @@ package kr.mycompany.myfinal;
 
 public class TestDTO {
 	private String pp;
+	private String dd;
 	public void process() {
 		System.out.println("유성은");
+	
+	}
+	public TestDTO() {
 	}
 }
