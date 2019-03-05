e04aa54426258400289859b0ac17aea526ee2d8b
user2 <user2@KH_G>
2019-03-05 21:12:57 +0900

    하핫

--- finalproject/src/main/java/kr/mycompany/myfinal/TestDTO.java
+++ finalproject/src/main/java/kr/mycompany/myfinal/TestDTO.java
@@ -3,8 +3,11 @@ package kr.mycompany.myfinal;
 public class TestDTO {
 	private String pp;
 	private String dd;
-	
 	public TestDTO() {
 		// TODO Auto-generated constructor stub
+	}
+	public void process() {
+		System.out.println("유성은");
+
 	}
 }
--- finalproject/src/main/java/test/Test.java
+++ finalproject/src/main/java/test/Test.java
@@ -2,4 +2,8 @@ package test;
 
 public class Test {
 
+	public void message() {
+		System.out.println("테스트 중");
+	}
+
 }
