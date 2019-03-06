676e953d9e0de4968ee95825a5e1ca48f65d3fac
Sonkih <rlghks1992@naver.com>
2019-03-06 15:37:03 +0900

    손기

--- finalproject/src/main/java/test/Test.java
+++ finalproject/src/main/java/test/Test.java
@@ -1,27 +1,12 @@
 package test;
 
-<<<<<<< HEAD
+
 public class Test {
-=======
-public class Test {
-
-<<<<<<< HEAD
-
-=======
->>>>>>> refs/remotes/origin/Nara
->>>>>>> refs/remotes/origin/master
-	public void message() {
-		System.out.println("테스트 중");
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-	}
-
 
 	public void message() {
 		System.out.println("테스트 중");
-=======
->>>>>>> refs/remotes/origin/Nara
->>>>>>> refs/remotes/origin/master
+
+	
+
 	}
 }
